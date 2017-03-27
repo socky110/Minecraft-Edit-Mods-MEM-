1253266b4bb370a7076b8fb622f9e96bbf54d6a2
socky110 <socky110@gmail.com>
2017-03-27 13:06:37 -0700

    Create glassdrop.java

---
+++ glassdrop.java
@@ -0,0 +1,11 @@
+package net.Cristalore.mod;
+
+import net.minecraftforge.event.world.BlockEvent;
+
+public class glassDrop {
+
+@ForgeSubscribe
+public void blockBreak(BlockEvent.BreakEvent event)
+{
+
+}
